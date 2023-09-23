b5818666963c27e866225749024636fcece0315e
German Gonzalez <german.glz01@gmail.com>
2023-09-23 02:39:40 -0600

    migration from css to scss preprocesor

--- src/app/pages/share-ideas/share-ideas.component.ts
+++ src/app/pages/share-ideas/share-ideas.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-share-ideas',
   templateUrl: './share-ideas.component.html',
-  styleUrls: ['./share-ideas.component.css']
+  styleUrls: ['./share-ideas.component.scss']
 })
 export class ShareIdeasComponent {
 
