46d99cafd99ddd8313344b76b7736111e4aa125d
German Gonzalez <german.glz01@gmail.com>
2023-09-25 20:48:57 -0600

    changing structure to allow routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './auth/login/login.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingComponent},
+  { path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  usuario: string = '';
+  contrasena: string = '';
 
+  enviar() {
+    console.log('Usuario:', this.usuario);
+    console.log('Contraseña:', this.contrasena);
+  }
 }
