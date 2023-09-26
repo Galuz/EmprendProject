5fcbe7e0d028b5d5e52121eda114284f305cbb7d
German Gonzalez <german.glz01@gmail.com>
2023-09-25 19:52:03 -0600

    functionality has been added to contact-form

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -13,6 +13,21 @@ export class ContactFormComponent {
   mensaje: string = '';
 
   enviarDatos() {
-    // Aquí puedes implementar la lógica para enviar los datos
+    // Crea el cuerpo del correo con los datos del formulario
+    const cuerpoCorreo = `Nombre: ${this.nombre}%0D%0A`
+      + `Correo Electrónico: ${this.correo}%0D%0A`
+      + `Teléfono: ${this.telefono}%0D%0A`
+      + `Fecha: ${this.fecha}%0D%0A`
+      + `Mensaje: ${this.mensaje}`;
+
+    // Abre el cliente de correo del usuario con los datos prellenados
+    window.location.href = `mailto:german.glz01@gmail.com?subject=Consulta&body=${cuerpoCorreo}`;
+
+    // Limpia los campos del formulario
+    this.nombre = '';
+    this.correo = '';
+    this.telefono = '';
+    this.fecha = '';
+    this.mensaje = '';
   }
 }
