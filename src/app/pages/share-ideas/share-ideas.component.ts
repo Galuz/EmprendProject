34f0a093152e7fc89175446eac00d972f88a0468
German Gonzalez <german.glz01@gmail.com>
2023-09-25 21:45:18 -0600

    share ideas has been added.

--- src/app/pages/share-ideas/share-ideas.component.ts
+++ src/app/pages/share-ideas/share-ideas.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./share-ideas.component.scss']
 })
 export class ShareIdeasComponent {
-
+  ideas = [
+    {
+      texto: 'Esta es una idea genial',
+      fecha: '2023-09-25',
+      usuario: 'Usuario 1'
+    },
+    {
+      texto: 'Otra idea interesante',
+      fecha: '2023-09-24',
+      usuario: 'Usuario 2'
+    },
+    // Agrega más ideas aquí
+  ];
 }
