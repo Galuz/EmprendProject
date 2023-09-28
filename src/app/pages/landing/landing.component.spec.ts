8fccf6f179bd1f0a5f587d14b1908afcc6f3865f
German Gonzalez <german.glz01@gmail.com>
2023-09-28 12:15:11 -0600

    fix some test errors

--- src/app/pages/landing/landing.component.spec.ts
+++ src/app/pages/landing/landing.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NavbarComponent } from 'src/app/navigation/navbar/navbar.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LandingComponent } from './landing.component';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
+import { LocationCardComponent } from '../location-card/location-card.component';
+import { FooterComponent } from 'src/app/navigation/footer/footer.component';
+import { CardComponent } from '../card/card.component';
+import { FormsModule } from '@angular/forms';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,7 +14,8 @@ describe('LandingComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LandingComponent]
+      declarations: [LandingComponent, NavbarComponent, FooterComponent, ContactFormComponent, LocationCardComponent, CardComponent],
+      imports: [HttpClientTestingModule, FormsModule]
     });
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
--- src/app/pages/location-card/location-card.component.spec.ts
+++ src/app/pages/location-card/location-card.component.spec.ts
@@ -1,10 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LocationCardComponent } from './location-card.component';
 
 describe('LocationCardComponent', () => {
   let component: LocationCardComponent;
   let fixture: ComponentFixture<LocationCardComponent>;
+
+  beforeAll(() => {
+    class MockMap {
+      setCenter = jasmine.createSpy('setCenter');
+    }
+    
+    class MockLatLng {
+      constructor(public lat: number, public lng: number) {}
+    }
+  
+    window['google'] = {
+      maps: {
+        ...window['google']?.maps,
+        Map: MockMap as any,
+        LatLng: MockLatLng as any,
+      } as any,
+    };
+  });
 
   beforeEach(() => {
     TestBed.configureTestingModule({
