8a17804ed8031516ccd25de3b4f35ffddd10db59
German Gonzalez <german.glz01@gmail.com>
2023-09-27 19:58:54 -0600

    landing page ts improved

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,20 +1,26 @@
 import { Component } from '@angular/core';
 
+interface CardData {
+  image: string;
+  title: string;
+  description: string;
+}
+
+const IMAGE_BASE_PATH = '../../../assets/images/profile-pic/';
+const DESCRIPTION = 'Es el Director de Desarrollo de Producto en nuestra startup. Es un apasionado de la tecnología y la innovación, y su experiencia en desarrollo de software es clave para la creación de productos de alta calidad.';
+
+const CARDS_DATA: CardData[] = [
+  { image: `${IMAGE_BASE_PATH}profile1.jpg`, title: 'Homero Archundia', description: DESCRIPTION },
+  { image: `${IMAGE_BASE_PATH}profile2.jpg`, title: 'Steven Spilberg', description: DESCRIPTION },
+  { image: `${IMAGE_BASE_PATH}profile3.jpg`, title: 'Guillo del Toro', description: DESCRIPTION }
+];
+
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
+
 export class LandingComponent {
-  cardsData = [
-    { image: '../../../assets/images/profile-pic/profile1.jpg', 
-      title: 'Homero Archundia', 
-      description: 'Es el Director de Desarrollo de Producto en nuestra startup. Es un apasionado de la tecnología y la innovación, y su experiencia en desarrollo de software es clave para la creación de productos de alta calidad.' },
-    { image: '../../../assets/images/profile-pic/profile2.jpg', 
-      title: 'Steven Spilberg', 
-      description: 'Es el Director de Desarrollo de Producto en nuestra startup. Es un apasionado de la tecnología y la innovación, y su experiencia en desarrollo de software es clave para la creación de productos de alta calidad.' },
-    { image: '../../../assets/images/profile-pic/profile3.jpg', 
-      title: 'Guillo del Toro', 
-      description: 'Es el Director de Desarrollo de Producto en nuestra startup. Es un apasionado de la tecnología y la innovación, y su experiencia en desarrollo de software es clave para la creación de productos de alta calidad.' }
-  ];
+  cardsData = CARDS_DATA;
 }
